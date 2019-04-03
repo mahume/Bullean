c748d3c9a6138b54a13d4f3d86ce6978dbfccdc1
mahume <michael.a.hume@gmail.com>
2019-04-03 11:23:17 -0600

    adding to

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
+require('dotenv').config();
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const db = require('./models');
 
 // Set up Express
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require('./models');
-
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+
+// Handlebars
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+  })
+);
+app.set('view engine', 'handlebars');
 
 // ROUTES
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
+const syncOptions = { force: true };
+
+if (process.env.NODE_ENV === 'test') {
+  syncOptions.force = true;
+}
+
 // Sync Sequelize Models, Start express app
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync(syncOptions).then(() => {
   app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
 });
+
+module.exports = app;
