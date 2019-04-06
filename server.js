1591cdf4d28d1780cca8863efc6befb659123563
mahume <michael.a.hume@gmail.com>
2019-04-05 18:30:04 -0600

    html skeleton

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const db = require('./models');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
