d7a43451569f7b2e0976a37ef352594f4f827211
mahume <michael.a.hume@gmail.com>
2019-04-06 13:46:56 -0600

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO crypto (coin_name, coin_symbol, purch_date, purch_price, purch_quantity)
+INSERT INTO Cryptos (coin_name, coin_symbol, purch_date, purch_price, purch_quantity)
 VALUES ('Bitcoin', 'BTC', '2017-01-13', 767.15, 10);
 
-INSERT INTO stocks (stock_name, stock_symbol, purch_date, purch_price, purch_quantity)
+INSERT INTO Stocks (stock_name, stock_symbol, purch_date, purch_price, purch_quantity)
 VALUES ('Alphabet', 'GOOGL', '2018-12-21', 991.25, 24);
 
-INSERT INTO real_estate (prop_address, purch_price, purch_date, rented, month_rev)
+INSERT INTO RealEstates (prop_address, purch_price, purch_date, rented, month_rev)
 VALUES ('123 AndrewBlows Rd Denver', 225000, '2018-12-21', true, 1600);
