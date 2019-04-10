3f9a9c460bec117e051956bc86fa4b12cdec7380
mahume <michael.a.hume@gmail.com>
2019-04-09 19:53:20 -0600

    colors

--- public/assets/js/doughnutChart.js
+++ public/assets/js/doughnutChart.js
@@ -3,11 +3,15 @@ const ctxDoughnut = document.getElementById('doughnutChart');
 const doughnutChart = new Chart(ctxDoughnut, {
   type: 'doughnut',
   data: {
-    labels: ['Crypto', 'Stocks', 'Real Estate'],
+    labels: ['Crypto', 'Real Estate', 'Stocks'],
     datasets: [
       {
         label: 'Investment types',
-        backgroundColor: ['#E18102', '#796BCB', '#2CB355'],
+        backgroundColor: [
+          'rgb(225, 184, 2)',
+          'rgb(225, 2, 84)',
+          'rgb(2, 165, 225)',
+        ],
         data: [2478, 5267, 734],
       },
     ],
