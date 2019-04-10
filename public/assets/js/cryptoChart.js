adbb40d38dee6edafb4eae661c873446e77c17f0
mahume <michael.a.hume@gmail.com>
2019-04-09 19:44:58 -0600

    color update

--- public/assets/js/cryptoChart.js
+++ public/assets/js/cryptoChart.js
@@ -1,8 +1,8 @@
 const crypto = document.getElementById('cryptoLineChart');
 
 const ctxCrypto = crypto.getContext('2d');
-const orangeGradient = ctxCrypto.createLinearGradient(100, 0, 500, 0);
-orangeGradient.addColorStop(0, 'rgba(225, 184, 2, 0.6)');
+const orangeGradient = ctxCrypto.createLinearGradient(0, 0, 0, 970);
+orangeGradient.addColorStop(0, 'rgba(225, 128, 2, 0.6)');
 orangeGradient.addColorStop(1, 'rgba(225, 184, 2, 0.6)');
 
 const cryptoLineChart = new Chart(ctxCrypto, {
