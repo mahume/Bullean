6112b105a2b546e426c2f5245a3c88035e9fe643
mahume <michael.a.hume@gmail.com>
2019-04-10 13:59:40 -0600

    border styling

--- public/assets/js/portfolioChart.js
+++ public/assets/js/portfolioChart.js
@@ -3,8 +3,8 @@ const portfolioChart = document.getElementById('barChart');
 const ctxPortfolio = portfolioChart.getContext('2d');
 // Crypto Gradient
 const cryptoPortfolioGradient = ctxPortfolio.createLinearGradient(0, 0, 0, 970);
-cryptoPortfolioGradient.addColorStop(0, 'rgba(225, 128, 2, 0.5)');
-cryptoPortfolioGradient.addColorStop(1, 'rgba(225, 184, 2, 0.5)');
+cryptoPortfolioGradient.addColorStop(0, 'rgba(225, 128, 2, 0.6)');
+cryptoPortfolioGradient.addColorStop(1, 'rgba(225, 184, 2, 0.6)');
 // Real Estate Gradient
 const realEstatePortfolioGradient = ctxPortfolio.createLinearGradient(
   0,
@@ -12,16 +12,16 @@ const realEstatePortfolioGradient = ctxPortfolio.createLinearGradient(
   0,
   970
 );
-realEstatePortfolioGradient.addColorStop(0, 'rgba(225, 2, 195, 0.5)');
-realEstatePortfolioGradient.addColorStop(1, 'rgba(225, 2, 84, 0.5)');
+realEstatePortfolioGradient.addColorStop(0, 'rgba(225, 2, 195, 0.6)');
+realEstatePortfolioGradient.addColorStop(1, 'rgba(225, 2, 84, 0.6)');
 // Stocks Gradient
 const stocksPortfolioGradient = ctxPortfolio.createLinearGradient(0, 0, 0, 970);
-stocksPortfolioGradient.addColorStop(0, 'rgba(32, 2, 225, 0.5)');
-stocksPortfolioGradient.addColorStop(1, 'rgba(2, 165, 225, 0.5)');
+stocksPortfolioGradient.addColorStop(0, 'rgba(32, 2, 225, 0.6)');
+stocksPortfolioGradient.addColorStop(1, 'rgba(2, 165, 225, 0.6)');
 
 window.addEventListener('load', () => {
   const portfolioChart = new Chart(ctxPortfolio, {
-    type: 'bar',
+    type: 'line',
     data: {
       labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
       datasets: [
@@ -29,46 +29,40 @@ window.addEventListener('load', () => {
           label: 'Crypto',
           type: 'line',
           data: [5, 10, 15, 25, 12, 3],
-          borderColor: cryptoPortfolioGradient,
+          borderColor: 'white',
           backgroundColor: cryptoPortfolioGradient,
           pointBorderColor: cryptoPortfolioGradient,
           pointBackgroundColor: cryptoPortfolioGradient,
           pointHoverBackgroundColor: cryptoPortfolioGradient,
           pointHoverBorderColor: cryptoPortfolioGradient,
-          borderWidth: 1,
+          borderWidth: 2,
           fill: true,
         },
         {
           label: 'Real Estate',
           type: 'line',
-          borderColor: realEstatePortfolioGradient,
+          borderColor: 'white',
           backgroundColor: realEstatePortfolioGradient,
           pointBorderColor: realEstatePortfolioGradient,
           pointBackgroundColor: realEstatePortfolioGradient,
           pointHoverBackgroundColor: realEstatePortfolioGradient,
           pointHoverBorderColor: realEstatePortfolioGradient,
           data: [10, 40, 80, 30, 10, 50],
-          borderWidth: 1,
+          borderWidth: 2,
           fill: true,
         },
         {
           label: 'Stocks',
           type: 'line',
-          borderColor: stocksPortfolioGradient,
+          borderColor: 'white',
           backgroundColor: stocksPortfolioGradient,
           pointBorderColor: stocksPortfolioGradient,
           pointBackgroundColor: stocksPortfolioGradient,
           pointHoverBackgroundColor: stocksPortfolioGradient,
           pointHoverBorderColor: stocksPortfolioGradient,
           data: [90, 10, 30, 40, 50, 10],
-          borderWidth: 1,
+          borderWidth: 2,
           fill: true,
-        },
-        {
-          label: 'Total',
-          type: 'bar',
-          backgroundColor: 'rgba(0,0,0,0.2)',
-          data: [10, 20, 30, 40, 50, 60],
         },
       ],
     },
