f7f9af54c29b58e301c3fcf9879a710714012168
mahume <michael.a.hume@gmail.com>
2019-04-11 12:01:23 -0600

    functionality for AJAX put

--- public/assets/js/crypto.js
+++ public/assets/js/crypto.js
@@ -7,22 +7,24 @@ const cryptoQuantity = $('#cryptoQuantity');
 const cryptoSubmitBtn = $('#cryptoSubmitBtn');
 
 const cryptoAPI = {
-  save: crypto =>
-    $.ajax({
+  save(crypto) {
+    return $.ajax({
       headers: {
         'Content-Type': 'application/json',
       },
       type: 'POST',
       url: 'api/crypto',
       data: JSON.stringify(crypto),
-    }),
-  get: () =>
-    $.ajax({
+    });
+  },
+  get() {
+    return $.ajax({
       url: 'api/crypto',
       type: 'GET',
-    }),
-  delete: id => {
-    $.ajax({
+    });
+  },
+  delete(id) {
+    return $.ajax({
       url: `api/crypto/${id}`,
       type: 'DELETE',
     });
@@ -32,15 +34,28 @@ const cryptoAPI = {
 const submitCrypto = event => {
   event.preventDefault();
 
+  const transaction = cryptoTransactionType[0].value;
   const cryptoData = {
-    transaction_date: cryptoDate,
+    transaction_date: cryptoDate[0].value,
     coin_name: cryptoName.val().trim(),
     coin_symbol: cryptoSymbol.val().trim(),
-    coin_price: cryptoPrice.val().trim(),
-    coin_quantity: cryptoQuantity.val().trim(),
+    coin_price: parseInt(cryptoPrice.val().trim()),
+    coin_quantity: parseInt(cryptoQuantity.val().trim()),
   };
 
-  cryptoAPI.save(cryptoData).then(() => {});
+  console.log(cryptoData);
+
+  switch (transaction) {
+    case 'purchase':
+      cryptoAPI.save(cryptoData).then(() => {
+        console.log('submitted');
+      });
+      break;
+    case 'sale':
+      break;
+    default:
+      break;
+  }
 };
 
-$(cryptoSubmitBtn).on('submit', submitCrypto);
+cryptoSubmitBtn.on('click', submitCrypto);
